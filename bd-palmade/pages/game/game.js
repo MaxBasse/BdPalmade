94d5249130a6bb17661eb1531778870dd15ebfe2
Max <maxxbasse@gmail.com>
2024-01-10 22:09:18 +0100

    ,zpngpzovizbvp

--- bd-palmade/pages/game/game.js
+++ bd-palmade/pages/game/game.js
@@ -1,14 +1,14 @@
 import style from '../../styles/Home.module.css';
 import styles from '../../styles/Game.module.css';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { useSearchParams } from 'next/navigation';
 import { Unity, useUnityContext } from "react-unity-webgl";
-import { Suspense } from 'react';
+import { useState, useEffect, useCallback, Suspense } from 'react';
 import {MenuItem} from '../../PageComps/pagecomps'
-
-
+import { sql } from '@vercel/postgres';
 
 /*function App() {
-  const [received, setReceived] = useState(0)
+  
 
  
 
@@ -51,12 +51,33 @@ import {MenuItem} from '../../PageComps/pagecomps'
 
 export default App;*/
 
+async function insert(email, score) {
+  /*try {
+    const { rows } = await sql`INSERT INTO scoreboard VALUES (${email,score})`;
+} catch (e){
+   console.log("Could not fetch posts from database", e);
+}*/
+
+const pool = createPool({
+  connectionString: process.env.SOME_POSTGRES_CONNECTION_STRING,
+});
+
+}
+  
+
+
+
+
 export default function Game() {
 
+  const params = useSearchParams();
 
+  const [received, setReceived] = useState(0)
   
+  
+
   const handle = useFullScreenHandle();
-  
+   
   const { unityProvider, sendMessage, addEventListener, removeEventListener, loadingProgression,isLoaded } = useUnityContext({
     loaderUrl: "../UnityGame/Build/UnityGame.loader.js",
     frameworkUrl: "../UnityGame/Build/UnityGame.framework.js",
@@ -64,23 +85,22 @@ export default function Game() {
     codeUrl: "../UnityGame/Build/UnityGame.wasm"
   });
 
-  /*const handleUnitySendScore = useCallback((score) => {
-    console.log("score : ", score)
-
-   
-    setReceived(score)
-  }, []);*/
-
-
-
-
- 
-  /*useEffect(() => {
+  const handleUnitySendScore = useCallback((score) => {
+    
+    if(score>received) {
+      setReceived(score)
+      console.log("score : ") 
+      insert(params.get("email"), score)
+      setReceived(0)
+    }
+    
+  }, []);
+  useEffect(() => {
       addEventListener("TransfertScore", handleUnitySendScore);
     return () => {
         removeEventListener("TransfertScore", handleUnitySendScore);
     };
-  }, [addEventListener, removeEventListener, handleUnitySendScore]);*/
+  }, [addEventListener, removeEventListener, handleUnitySendScore]);
       
   
       
@@ -93,7 +113,8 @@ export default function Game() {
                   <MenuItem title="OUI" link="https://google.com"/>
                 </div>
               </header>
-              <button className={styles.fscreenbutton} onClick={handle.enter}>Plein Écran</button>
+              <button className={styles.fscreenbutton} onClick={handle.enter}>Plein Écran {received}</button>
+
               <Unity className={styles.unity} unityProvider={unityProvider} />  
         
 
